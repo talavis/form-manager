60b2106770ced412f658c9de09f8fd4696f84450
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2022-05-29 23:17:03 +0200

    Get version info from environment variable VERSION

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -52,7 +52,9 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-build
     build: {
       vueRouterMode: 'history', // available values: 'hash', 'history'
-
+      env: {
+        VERSION: process.env.VERSION,
+      },
       // transpile: false,
       // publicPath: '/',
 
